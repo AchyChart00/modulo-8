d4751f28eeacd3c55c8b22a60c7c0a611d9bf131
Jacob <jacob.sanchezs@outlook.com>
2024-02-01 15:47:53 -0600

    Hateoas added

---
+++ WebApiAutores/Controllers/RootController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.DTOs;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = "ObtenerRoot")]
+        public ActionResult<IEnumerable<DatosHATEOAS>> Get()
+        {
+            var datosHateoas = new List<DatosHATEOAS>();
+
+            datosHateoas.Add(new DatosHATEOAS(
+                enlace : Url.Link("ObtenerRoot", new { }), 
+                descripcion:"self", 
+                metodo: "GET"));
+
+            return datosHateoas;
+        }
+    }
+}
